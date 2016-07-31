757de4ee9f583134fd4eb3f82012520dcd80fe96
bfv <dev@bfv.io>
2016-07-31 17:39:09 +0200

    implemented settings

--- app/pages/settings/settings.component.ts
+++ app/pages/settings/settings.component.ts
@@ -1,14 +1,39 @@
 
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, ViewChild, ElementRef} from "@angular/core";
+import {Settings} from "../../shared/settings";
+import {View} from "ui/core/view";
+import {Page} from "ui/page";
+import {SettingsService} from "../../shared/settings.service";
 
 @Component({
     selector: "settingspage",
-    templateUrl: "pages/settings/settings.component.html"
+    providers: [SettingsService],
+    templateUrl: "pages/settings/settings.component.html",
+    styleUrls: ["pages/settings/settings.css"]
 })
 
 export class SettingsPage implements OnInit {
 
-    ngOnInit() {
+    @ViewChild("authContainer") authContainer: ElementRef;
+    @ViewChild("otherContainer") otherContainer: ElementRef;
 
+    public settings: Settings;
+
+    constructor(private page: Page, private settingsService: SettingsService) {
+
+    }
+
+    ngOnInit() {
+        this.settings = this.settingsService.get();
+    }
+
+    authEnabledChanged(event: Event) {
+        console.log("1.0.12 authEnabled changed to: " + this.settings.authEnabled);
+    }
+
+    confirmSettings() {
+
+        // todo: solve this with a promise
+        this.settingsService.save(this.settings);
     }
 }
