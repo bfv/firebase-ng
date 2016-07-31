b68c3110f70ede83e8bfedfd603df7b51ae88fd0
bfv <dev@bfv.io>
2016-07-31 17:38:21 +0200

    created settings (service)

---
+++ app/shared/settings.service.ts
@@ -0,0 +1,41 @@
+import {Injectable} from "@angular/core";
+import applicationSettings = require("application-settings");
+import {Settings} from "./settings";
+
+@Injectable()
+export class SettingsService {
+
+    private settings: Settings;
+
+
+    constructor() {
+        this.settings = new Settings();
+        this.fetchSettings(); 
+    }
+
+    private fetchSettings() {
+        
+        if (applicationSettings.hasKey("authEnabled")) {
+            this.settings.authEnabled = applicationSettings.getBoolean("authEnabled");
+        }
+
+        if (applicationSettings.hasKey("username")) {
+            this.settings.username = applicationSettings.getString("username");
+        }
+
+        if (applicationSettings.hasKey("password")) {
+            this.settings.password = applicationSettings.getString("password");
+        }
+    }
+
+    get(): Settings {
+        return this.settings;
+    }
+
+    save(newSettings: Settings) {
+        applicationSettings.setBoolean("authEnabled", newSettings.authEnabled);
+        applicationSettings.setString("username", newSettings.username);
+        applicationSettings.setString("password", newSettings.password);
+        this.settings = newSettings;
+    }
+}
---
+++ app/shared/settings.ts
@@ -0,0 +1,9 @@
+
+export class Settings {
+    
+    // authentication
+    public authEnabled = true;
+    public username: string = "";
+    public password: string = "";
+
+}
