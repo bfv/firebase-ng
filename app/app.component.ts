e2b1f9faa94e54d95d8db89e32c3d6af2364bf87
bfv <dev@bfv.io>
2016-08-05 07:04:29 +0200

    #3 firebase initialization

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {NS_ROUTER_DIRECTIVES} from "nativescript-angular/router";
+import firebase = require("nativescript-plugin-firebase");
 
 @Component({
   selector: "firebaseng",
@@ -7,4 +8,29 @@ import {NS_ROUTER_DIRECTIVES} from "nativescript-angular/router";
   template: "<page-router-outlet></page-router-outlet>"
 })
 
-export class AppComponent { }
+export class AppComponent implements OnInit { 
+
+  ngOnInit() {
+    
+    let firebaseOptions = {
+        persist: false
+    };
+
+    firebase.init(firebaseOptions).then(
+        instance => {
+            console.log("firebase init: OK");
+
+            // if the push token is returned everything works
+            firebase.addOnPushTokenReceivedCallback(
+                token => { console.log("push token: " + token); }
+            );
+            
+        },
+        error => {
+            console.log("firebase init error: " + error);
+        }
+    );
+
+  }
+
+}
