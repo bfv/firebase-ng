eb1ae55fe81a01b5ba91e9ac6fbe284c21b9073d
bfv <dev@bfv.io>
2016-08-05 13:38:37 +0200

    #4 implemented logging in on start

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,36 +1,46 @@
 import {Component, OnInit} from "@angular/core";
 import {NS_ROUTER_DIRECTIVES} from "nativescript-angular/router";
 import firebase = require("nativescript-plugin-firebase");
+//import {SettingsService} from "./shared/settings.service";
+import {ApplicationService} from "./shared/application.service";
+
 
 @Component({
-  selector: "firebaseng",
-  directives: [NS_ROUTER_DIRECTIVES],
-  template: "<page-router-outlet></page-router-outlet>"
+    selector: "firebaseng",
+    directives: [NS_ROUTER_DIRECTIVES],
+    providers: [ApplicationService], 
+    template: "<page-router-outlet></page-router-outlet>"
 })
 
-export class AppComponent implements OnInit { 
+export class AppComponent implements OnInit {
 
-  ngOnInit() {
-    
-    let firebaseOptions = {
-        persist: false
-    };
+    constructor(private applicationService: ApplicationService) {
 
-    firebase.init(firebaseOptions).then(
-        instance => {
-            console.log("firebase init: OK");
+    }
 
-            // if the push token is returned everything works
-            firebase.addOnPushTokenReceivedCallback(
-                token => { console.log("push token: " + token); }
-            );
-            
-        },
-        error => {
-            console.log("firebase init error: " + error);
-        }
-    );
+    ngOnInit() {
 
-  }
+        let firebaseOptions = {
+            persist: false
+        };
 
+        firebase.init(firebaseOptions).then(
+            instance => {
+                console.log("firebase init: OK");
+
+                // if the push token is returned everything works
+                firebase.addOnPushTokenReceivedCallback(
+                    token => { console.log("push token: " + token); }
+                );
+
+                console.log("attempt to login");
+                this.applicationService.login();
+
+            },
+            error => {
+                console.log("firebase init error: " + error);
+            }
+        );
+
+    }
 }
--- app/main.ts
+++ app/main.ts
@@ -3,8 +3,15 @@ import {nativeScriptBootstrap} from "nativescript-angular/application";
 import {AppComponent} from "./app.component";
 import {APP_ROUTER_PROVIDERS} from "./app.routes";
 
+// setup dependencies for ApplicationService
+import {AuthService} from "./shared/auth.service";
+import {SettingsService} from "./shared/settings.service";
+
+
 let providers = [
-    APP_ROUTER_PROVIDERS
+    APP_ROUTER_PROVIDERS,
+    AuthService,
+    SettingsService
 ];
 
 nativeScriptBootstrap(AppComponent, providers);
